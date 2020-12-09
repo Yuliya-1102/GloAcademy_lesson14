dff0319fc5e2d6e6f443adbd94c07164ea2fc00e
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-12-09 14:15:10 +0300

    add task lesson 15/2

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 'use strict';
 
-function DomElement(selector, width, height, bg, fontSize, text){
-    this.newElement = function(){
-       let element;
-       if(selector[0] === '.'){
-            element = document.createElement('div');
-            element.classList.add(selector.slice(1));
-            document.querySelector('body').append(element);
-        } else if(selector[0] === '#'){
-            element = document.createElement('p');
-            element.id = selector.slice(1);
-            document.querySelector('body').append(element);
-        }
-        element.textContent = text;
-        element.style.width = width;
-        element.style.height = height;
-        element.style.background = bg;
-        element.style.fontSize = fontSize;
-    };
+class First {
+    hello(){
+        console.log("Привет я метод родителя!");
+    }
 }
 
-let obj = new DomElement('.block', '200px', '200px', 'red', '25px', 'Hello world');
-let obj2 = new DomElement('#paragraph', '200px', '200px', 'yellow', '25px', 'Hello syn');
+class Second extends First {
+    hello(){
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
 
-obj.newElement();
-obj2.newElement();
-// git remote add origin https://github.com/Yuliya-1102/GloAcademy_lesson14.git
+const obj2 = new Second();
+obj2.hello();
