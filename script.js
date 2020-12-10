66f25f95773c2bb5bc7ddc306e74d3fa46dc58e6
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-12-10 22:28:25 +0300

    add task lesson14/hard

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 'use strict';
 
-function DomElement(selector, width, height, bg, fontSize, text){
+function DomElement(width, height, bg, position){
     this.newElement = function(){
-       let element;
-       if(selector[0] === '.'){
-            element = document.createElement('div');
-            element.classList.add(selector.slice(1));
-            document.querySelector('body').append(element);
-        } else if(selector[0] === '#'){
-            element = document.createElement('p');
-            element.id = selector.slice(1);
-            document.querySelector('body').append(element);
-        }
-        element.textContent = text;
+        let element  = document.createElement('div');
+        element.classList.add('square');
+        document.querySelector('body').append(element);
         element.style.width = width;
         element.style.height = height;
         element.style.background = bg;
-        element.style.fontSize = fontSize;
+        element.style.position = position;
     };
 }
 
-let obj = new DomElement('.block', '200px', '200px', 'red', '25px', 'Hello world');
-let obj2 = new DomElement('#paragraph', '200px', '200px', 'yellow', '25px', 'Hello syn');
+let obj = new DomElement('100px', '100px', 'red', 'absolute');
+document.addEventListener("DOMContentLoaded", obj.newElement());
 
-obj.newElement();
-obj2.newElement();
-// git remote add origin https://github.com/Yuliya-1102/GloAcademy_lesson14.git
+let square = document.querySelector('.square');
+let styleSquare = document.querySelector('.square').style;
+
+let keyBottomTop = 10;
+let keyLeftRight = 10;
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowUp'){
+        keyBottomTop -= 10;
+        styleSquare.top = keyBottomTop + "px";
+    } else if(event.key === 'ArrowDown'){
+        keyBottomTop += 10;
+        styleSquare.top = keyBottomTop + "px";
+    } else if(event.key === 'ArrowRight'){
+        keyLeftRight += 10;
+        styleSquare.left = keyLeftRight + "px";
+    } else if(event.key === 'ArrowLeft'){
+        keyLeftRight -= 10;
+        styleSquare.left = keyLeftRight + "px";
+    }
+});
